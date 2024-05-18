d7c4007770b18ce013d9851b15ab1e5d2fad399a
Angelika Maglinte <53634332+angelikamaglinte@users.noreply.github.com>
2024-05-17 22:59:26 -0600

    Modified auth.js to include user login api

--- routes/auth.js
+++ routes/auth.js
@@ -115,6 +115,50 @@ router.get('/verify-email', async (req, res) => {
   }
 });
 
+// User login
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if user exists
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+
+    // Check if password is correct
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+
+    // Check if user is verified
+    if (!user.isVerified) {
+      return res.status(400).json({ msg: 'Email not verified' });
+    }
+
+    // Generate JWT
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 
 
 module.exports = router;
