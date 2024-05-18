f961b10932f540b249e1c3b11517b9e12788daf4
Angelika Maglinte <53634332+angelikamaglinte@users.noreply.github.com>
2024-05-17 23:29:29 -0600

    Update to include CORS

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
 // Middleware
+app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
 
 // Connect to MongoDB
